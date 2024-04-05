00fd50682c78e421fbdc8de583f8475bee874a94
Junjie Fang <fang.junj@northeastern.edu>
2024-04-05 10:28:59 -0700

    explore main menu

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,20 +4,18 @@ using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
     public GameObject infoDialog; // Assign this in the inspector within Info Dialog Panel
-
+    public Animation anim;
     void Start()
     {
         // Ensure the dialog is initially hidden
         infoDialog.SetActive(false);
+        anim = FindObjectOfType<Animation>();
     }
     // Toggle function for the dialog
     public void ToggleInfoDialog()
     {
         if (infoDialog != null)
+            anim.Play("Window-In");
             infoDialog.SetActive(!infoDialog.activeSelf); // Toggle the active state
-    }
-    public void LoadLevelSelect()
-    {
-        SceneManager.LoadScene("LevelSelectMenu"); 
     }
 }
